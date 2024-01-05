ba648947a48914a6559ac1e187c8434479424f25
aahmadzf <aahmadzf0@gmail.com>
2024-01-05 19:55:19 +0700

    Update WasteTabel

--- Waste/WasteTableModel.java
+++ Waste/WasteTableModel.java
@@ -1,5 +1,65 @@
 package Waste;
 
+import java.lang.reflect.Waste;
+import java.util.List;
+
 public class WasteTableModel {
-    
+
+    private String[] columnNames = { "Nama", "Jenis Waste" };
+    private List<Waste> data;
+
+    public WasteTableModel(List<Waste> data) {
+        this.data = data;
+    }
+
+    public int getColumnCount() {
+        return columnNames.length;
+    }
+
+    public int getRowCount() {
+        return data.size();
+    }
+
+    public String getColumnName(int col) {
+        return columnNames[col];
+    }
+
+    public Object getValueAt(int row, int col) {
+        Waste rowItem = data.get(row);
+        String value = "";
+
+        switch (col) {
+            case 0:
+                value = rowItem.getNama();
+                break;
+            case 1:
+                value = rowItem.getJenisWaste().getNama();
+                break;
+        }
+        return value;
+    }
+
+    public boolean isCellEditable(int row, int col) {
+        return false;
+    }
+
+    public void add(Waste value) {
+        data.add(value);
+        fireTableRowsInserted(data.size() - 1, data.size() - 1);
+    }
+
+    public void update(Waste update, int selected) {
+        data.set(selected, update);
+        fireTableCellUpdated(selected, 0);
+        fireTableCellUpdated(selected, 1);
+    }
+
+    public void remove(int rowIndex) {
+        if (rowIndex >= 0 && rowIndex < data.size()) {
+            data.remove(rowIndex);
+            fireTableRowsDeleted(rowIndex, rowIndex); // Wastei tahu JTable bahwa baris telah dihapus
+        } else {
+            throw new IndexOutOfBoundsException("Index tidak valid");
+        }
+    }
 }
